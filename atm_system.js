755eca78ed997a857ad9439017e4c895136fcc13
clarissatiffany <163639449+clarissatiffany@users.noreply.github.com>
2024-11-28 05:15:30 +0000

    Create account, change password

--- atm_system.js
+++ atm_system.js
@@ -1,5 +1,70 @@
+
 const prompt = require('prompt-sync')();
 const fs = require('fs');
+
+//Fungsi untuk membuat akun
+function createacc() {
+    
+  let name = prompt("Input your name:");
+  while (!name) {
+    console.log("Kindly try inputing the name again.");
+    name = prompt("Input your name:");
+  }
+
+  let email = prompt("Input your email address:");
+  while (!email || !emailvalid(email)) {
+    console.log("Kindly provide a valid email address.");
+    email = prompt("Input your email address:");
+  }
+
+  let password = "";
+  while (password.length < 5) {
+    password = prompt("Create a strong password of at least 5 characters:");
+    if (password.length < 5) {
+      console.log("Password must consists of at least 5 characters.");
+      return;
+    }
+  }
+
+  let confirmpassw = "";
+  while (confirmpassw !== password) {
+    confirmpassw = prompt("Confirm your password:");
+    if (confirmpassw !== password) {
+      console.log("Password is different, please try inputing again.");
+    }
+  }
+
+  console.log("Account successfully activated. Welcome, " + name + "!");
+}
+
+function emailcheck(email) {
+  const emailchar = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailchar.test(email);
+}
+
+function passwchange(account) {
+  let password = prompt("Enter your previous password:");
+  if (password !== account.password) {
+    console.log("Incorrect password. Please enter the password correctly.");
+    return; 
+  }
+
+  let passwnew = prompt("New password (at least be 5 characters)):");
+  while (passwnew.length !== 4 || isNaN(passwnew)) {
+    console.log("Invalid password, password must be at least 5 characters.");
+    passwnew = prompt("New password (at least be 5 characters)):");
+  }
+
+  let passwconfirm = prompt("Confirm your new password:");
+  while (passwconfirm !== passwnew) {
+    console.log("Passwords are different, kindly try again.");
+    passwconfirm = prompt("Confirm your new passwrd:");
+  }
+
+  account.pin = passwnew; 
+  console.log("PIN change successful!");
+}
+
 
 // Fungsi untuk membaca data akun dari file
 function loadAccounts() {
@@ -152,3 +217,4 @@ function main() {
 
 // Jalankan program utama
 main();
+createacc();
